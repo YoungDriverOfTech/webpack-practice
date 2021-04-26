e5b595c2b10ec3b61b4bb3cdc0c9206b614c528b
liang <xiaoleliang045@gmail.com>
2021-04-26 23:33:59 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development", // "production" | "development" | "none"
+    entry: "./src/index.js", // string | object | array
+    // defaults to ./src
+    // Here the application starts executing
+    // and webpack starts bundling
+    output: {
+      // options related to how webpack emits results
+      path:path.resolve(__dirname, "dist"), // string (default)
+      // the target directory for all output files
+      // must be an absolute path (use the Node.js path module)
+      filename: "[name].[contenthash].js", // string (default)
+    },
+}

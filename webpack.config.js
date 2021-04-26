177499f39ff33c18c9a8d9602597255e09fdbdec
liang <xiaoleliang045@gmail.com>
2021-04-27 00:15:16 +0900

    third commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: 'inline-source-map',
   output: {
     path:path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
+  },
+  devServer: {
+    contentBase: './dist',
   },
   plugins: [new HtmlWebpackPlugin({
     title: 'My App',
